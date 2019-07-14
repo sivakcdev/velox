18539d471db5a9f4b447aa09b8f44b4e2988937f
root <root@ip-172-31-93-132.ec2.internal>
2019-07-14 13:02:50 +0000

    velox

---
+++ src/main/java/com/velox/App.java
@@ -0,0 +1,13 @@
+package com.velox;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
